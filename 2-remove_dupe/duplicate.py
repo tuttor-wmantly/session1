a9be976e28d755458bcf3f7ef83018940dbf99c8
William Mantly <wmantly@gmail.com>
2019-06-18 23:14:12 -0400

    Update duplicate.py

--- 2-remove_dupe/duplicate.py
+++ 2-remove_dupe/duplicate.py
@@ -1,2 +1,13 @@
 def remove_duplicate(string):
-  pass
+	duplicates = ''
+	return_string = ''
+	last = string[0]
+
+	for letter in string[1:]:
+		if letter == last:
+			duplicates += letter
+		else:
+			return_string += letter
+		last = letter
+
+	return return_string, duplicates
